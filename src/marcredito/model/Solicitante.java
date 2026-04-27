14745065730f61f00d74ad1aca07a04ae06371c2
Carlos Rodriguez <84290592+TheMonster374@users.noreply.github.com>
2026-04-26 19:36:20 -0500

    Add comment to Solicitante class

--- src/marcredito/model/Solicitante.java
+++ src/marcredito/model/Solicitante.java
@@ -1,5 +1,6 @@
 package marcredito.model;
 
+// Solicitante: solicita préstamos y gestiona sus pagos.
 public class Solicitante extends Usuario {
     public Solicitante(String id, String nombre, String correo) {
         super(id, nombre, correo);
