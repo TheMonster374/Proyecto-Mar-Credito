8322e59b055cece3ffe03812c8f4caa96dec69ee
Carlos Rodriguez <84290592+TheMonster374@users.noreply.github.com>
2026-04-27 16:30:07 -0500

    Refactor Usuario class getters and toString method
    Updated toString method formatting and changed access modifiers for getter methods.

--- src/marcredito/model/Usuario.java
+++ src/marcredito/model/Usuario.java
@@ -1,5 +1,6 @@
 package marcredito.model;
 
+
 public abstract class Usuario {
     private final String id;
     private final String nombre;
@@ -13,14 +14,28 @@ public abstract class Usuario {
 
     @Override
     public String toString() {
-        return "Usuario{" +
-               "id='" + id + '\'' +
-               ", nombre='" + nombre + '\'' +
-               ", correo='" + correo + '\'' +
-               '}';
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", correo=" + correo + '}';
     }
 
-    public String getId() { return id; }
-    public String getNombre() { return nombre; }
-    public String getCorreo() { return correo; }
+    /**
+     * @return the id
+     */
+    protected String getId() {
+        return id;
+    }    
+
+    /**
+     * @return the nombre
+     */
+    protected String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * @return the correo
+     */
+    protected String getCorreo() {
+        return correo;
+    }
+    
 }
