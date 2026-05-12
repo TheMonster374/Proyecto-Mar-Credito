26f987f3ea7459f8ebe44f0f131804da2eeb35a5
Carlos Rodriguez <84290592+TheMonster374@users.noreply.github.com>
2026-05-12 18:26:58 -0500

    Refactor Prestamo class to use Acuerdo

--- src/marcredito/model/Prestamo.java
+++ src/marcredito/model/Prestamo.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Prestamo {
-    private final double monto;
-    private final int plazoMeses;
-    private final double interes;
+    Acuerdo acuerdo;
+    private final double monto;       
     private final Solicitante solicitante;
     private final Prestamista prestamista;
     private final List<Pago> pagos = new ArrayList<>();
@@ -15,15 +14,14 @@ public class Prestamo {
      * Crea un préstamo.
      *
      * @param monto monto prestado
-     * @param plazoMeses plazo en meses
-     * @param interes interés en porcentaje
+     * @param acuerdo
      * @param solicitante solicitante del préstamo
      * @param prestamista prestamista del préstamo
      */
-    public Prestamo(double monto, int plazoMeses, double interes, Solicitante solicitante, Prestamista prestamista) {
+    
+    public Prestamo(double monto, Acuerdo acuerdo, Solicitante solicitante, Prestamista prestamista) {    
         this.monto = monto;
-        this.plazoMeses = plazoMeses;
-        this.interes = interes;
+        this.acuerdo = acuerdo;
         this.solicitante = solicitante;
         this.prestamista = prestamista;
     }
@@ -43,7 +41,7 @@ public class Prestamo {
      * @return total a pagar
      */
     public double calcularTotalPagar() {
-        return monto + (monto * interes / 100);
+        return monto + (monto * acuerdo.getInteres() / 100);
     }
 
     /**
@@ -62,8 +60,8 @@ public class Prestamo {
      */
     public String resumen() {
         return "Prestamo: monto=" + monto
-                + ", plazo=" + plazoMeses
-                + " meses, interes=" + interes
+                + ", plazo=" + acuerdo.getPlazoMeses()
+                + " meses, interes=" + acuerdo.getInteres()
                 + "%, solicitante=" + solicitante.getNombre()
                 + ", prestamista=" + prestamista.getNombre()
                 + ", pagos registrados=" + pagos.size();
