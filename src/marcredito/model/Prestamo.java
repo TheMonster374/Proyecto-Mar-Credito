cd54dbc37fa6f7e59188bbad20f7b959dca95f2e
Carlos Rodriguez <84290592+TheMonster374@users.noreply.github.com>
2026-05-16 21:36:35 -0500

    Refactor Prestamo class and add ID generation

--- src/marcredito/model/Prestamo.java
+++ src/marcredito/model/Prestamo.java
@@ -2,68 +2,117 @@ package marcredito.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Prestamo {
+
+    private String idPrestamo;
+    private final double monto;
+    private String estado;
+
     private final Acuerdo acuerdo;
-    private final double monto;       
     private final Solicitante solicitante;
     private final Prestamista prestamista;
+
     private final List<Pago> pagos = new ArrayList<>();
 
     /**
      * Crea un préstamo.
      *
      * @param monto monto prestado
-     * @param acuerdo
-     * @param solicitante solicitante del préstamo
-     * @param prestamista prestamista del préstamo
+     * @param acuerdo acuerdo del préstamo
+     * @param solicitante solicitante
+     * @param prestamista prestamista
      */
-    
-    public Prestamo(double monto, Acuerdo acuerdo, Solicitante solicitante, Prestamista prestamista) {    
+    public Prestamo(double monto, Acuerdo acuerdo, Solicitante solicitante,  Prestamista prestamista) {
+
+        this.idPrestamo = generarId();
         this.monto = monto;
+
+        this.estado = "Pendiente";
+
         this.acuerdo = acuerdo;
         this.solicitante = solicitante;
         this.prestamista = prestamista;
     }
 
     /**
-     * Registra un pago en el préstamo.
-     *
-     * @param pago pago realizado
+     * Genera ID tipo P1234
+     */
+    private String generarId() {
+
+        Random random = new Random();
+
+        int numero = 1000 + random.nextInt(9000);
+
+        return "P" + numero;
+    }
+
+    /**
+     * Registrar pago
+     * @param pago
      */
     public void registrarPago(Pago pago) {
         pagos.add(pago);
     }
 
     /**
-     * Calcula el total a pagar con interés.
-     *
-     * @return total a pagar
+     * Total a pagar
+     * @return 
      */
     public double calcularTotalPagar() {
-        return monto + (monto * acuerdo.getInteres() / 100);
+
+        return monto +
+                (monto * acuerdo.getInteres() / 100);
     }
 
     /**
-     * Obtiene el solicitante.
-     *
-     * @return solicitante del préstamo
+     * Resumen
+     * @return 
      */
+    public String resumen() {
+
+        return "Prestamo: monto=" + monto
+                + ", plazo=" + acuerdo.getPlazoMeses()
+                + " meses, interes=" + acuerdo.getInteres()
+                + "%, estado=" + estado
+                + ", solicitante=" + solicitante.getNombre()
+                + ", prestamista=" + prestamista.getNombre();
+    }
+
+    public String getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public void setIdPrestamo(String idPrestamo) {
+        this.idPrestamo = idPrestamo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Acuerdo getAcuerdo() {
+        return acuerdo;
+    }
+
     public Solicitante getSolicitante() {
         return solicitante;
     }
 
-    /**
-     * Genera un resumen del préstamo.
-     *
-     * @return resumen del préstamo
-     */
-    public String resumen() {
-        return "Prestamo: monto=" + monto
-                + ", plazo=" + acuerdo.getPlazoMeses()
-                + " meses, interes=" + acuerdo.getInteres()
-                + "%, solicitante=" + solicitante.getNombre()
-                + ", prestamista=" + prestamista.getNombre()
-                + ", pagos registrados=" + pagos.size();
+    public Prestamista getPrestamista() {
+        return prestamista;
+    }
+
+    public List<Pago> getPagos() {
+        return pagos;
     }
 }
