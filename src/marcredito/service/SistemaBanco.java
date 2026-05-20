bd331162587692b22b77df5c1d34a68a734d7cc0
Carlos Rodriguez <84290592+TheMonster374@users.noreply.github.com>
2026-05-19 19:14:06 -0500

    Add files via upload

--- src/marcredito/service/SistemaBanco.java
+++ src/marcredito/service/SistemaBanco.java
@@ -9,20 +9,10 @@ public class SistemaBanco {
     private final List<Usuario> usuarios = new ArrayList<>();
     private final List<Prestamo> prestamos = new ArrayList<>();
 
-    /**
-     * Agrega un usuario al sistema.
-     *
-     * @param usuario usuario a registrar
-     */
     public void agregarUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
         
-    /**
-     * Busca un usuario dentro del sistema por su ID.
-     * @param id ID del usuario
-     * @return el Usuario si se encuentra; si no se encuentra, retorna null
-     */
     public Usuario buscarUsuarioPorId(String id) {
         for (Usuario u : usuarios) {
             if (u.getId().equals(id)) {
@@ -32,21 +22,10 @@ public class SistemaBanco {
         return null;
     }
 
-    /**
-     * Agrega un préstamo al sistema.
-     *
-     * @param prestamo préstamo a registrar
-     */
     public void agregarPrestamo(Prestamo prestamo) {
         prestamos.add(prestamo);
     }
 
-    /**
-     * Obtiene los préstamos asociados a un usuario.
-     *
-     * @param usuario usuario a consultar
-     * @return lista de préstamos del usuario
-     */
        public List<Prestamo> obtenerPrestamosDeUsuario(Usuario usuario) {
         List<Prestamo> prestamosUsuario = new ArrayList<>();
         for (Prestamo prestamo : prestamos) {
@@ -57,21 +36,10 @@ public class SistemaBanco {
         return prestamosUsuario;
     }
     
-    /**
-     * Carga usuarios registrados
-     * 
-     * @return 
-    */
-    
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
-    
-    /**
-     * Carga Prestamos creados
-     * 
-     * @return 
-     */
+
     public List<Prestamo> getPrestamos() {
         return prestamos;
     }
